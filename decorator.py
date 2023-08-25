d6a7149bebfcc4d6fbae66ab27b3c486e54da832
Godfrey <godfreypet@gmail.com>
2023-08-25 16:37:26 +0100

    decorator version 1

---
+++ decorator.py
@@ -0,0 +1,19 @@
+
+def smart(func):
+    def inner(a,b):
+        if a<b:
+          a,b = b,a
+          return
+        elif b == 0:
+          print("indivisible by o")
+          return
+        else:
+          print("error")
+        return func(a,b)
+    return inner
+          
+@smart
+def div(a,b):
+    print(a/b)
+
+div(2,0)
